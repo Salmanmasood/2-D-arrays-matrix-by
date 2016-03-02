59278723f2eb971a2d005c3b2fda884d104b702a
salman AL-FARSI <salmanmasood24@yahoo.com>
2016-03-01 23:12:32 -0800

    cas3 8 added

--- 2-D arrays/2-D arrays/class2.cs
+++ 2-D arrays/2-D arrays/class2.cs
@@ -26,7 +26,32 @@ namespace _2_D_arrays
                    }
 
                }
+           L1:
+               Console.ForegroundColor = ConsoleColor.Green;
+               Console.WriteLine("Press 'd' or 'D' to diplay the matrix A\nPress 'm' or 'M' to go in the main menu... ");
+               char d = char.Parse(Console.ReadLine());
+               Console.ForegroundColor = ConsoleColor.White;
+               if (d == 'd' || d == 'D')
+               {
+                   matrixoperation displayobj = new matrixoperation();
+                   char m = 'C';
+                   displayobj.display(c, ra, ca, m);
+                   mainclass ob2 = new mainclass();
+                   ob2.goback();
+               }
+               else if (d == 'm' || d == 'M')
+               {
+                   mainclass ob2 = new mainclass();
+                   ob2.goback();
+               }
+               else
+               {
+                   mainclass ob2 = new mainclass();
+                   ob2.invalidinput();
+                   goto L1;
+               }
 
+           
            }
            
            else
@@ -40,6 +65,68 @@ namespace _2_D_arrays
 
 
         }
+
+        public void subdisplay(int[,] a, int[,] b, int[,] c, int ra, int ca, int rb, int cb)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("============================Adding MATRIces============================");
+            Console.ForegroundColor = ConsoleColor.White;
+            class2 ob = new class2();
+            bool x = ob.cmpforadd(ra, ca, rb, cb);
+            if (x == true)
+            {
+                for (int i = 1; i < ra; i++)
+                {
+                    for (int j = 1; j < ca; j++)
+                    {
+                        c[i, j] = a[i, j] - b[i, j];
+
+                    }
+
+                }
+            L1:
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Press 'd' or 'D' to diplay the matrix A\nPress 'm' or 'M' to go in the main menu... ");
+                char d = char.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+                if (d == 'd' || d == 'D')
+                {
+                    matrixoperation displayobj = new matrixoperation();
+                    char m = 'C';
+                    displayobj.display(c, ra, ca, m);
+                    mainclass ob2 = new mainclass();
+                    ob2.goback();
+                }
+                else if (d == 'm' || d == 'M')
+                {
+                    mainclass ob2 = new mainclass();
+                    ob2.goback();
+                }
+                else
+                {
+                    mainclass ob2 = new mainclass();
+                    ob2.invalidinput();
+                    goto L1;
+                }
+
+         
+         
+            
+            
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Matrices cannot be Subtracted....");
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
+        }
+
+
+
 
         public bool cmpforadd(int ra,int ca,int rb,int cb)
         {
