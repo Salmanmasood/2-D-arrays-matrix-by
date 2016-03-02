86931aa2b4576520f40668fd268a3f804ec13cc2
salman AL-FARSI <salmanmasood24@yahoo.com>
2016-03-01 22:49:11 -0800

    started work!!! 1st commit

---
+++ 2-D arrays/2-D arrays/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_D_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flaga = 0,flagb=0;
+            int maininput;
+
+            main:
+            mainclass ob = new mainclass();
+            try
+            {
+                ob.mainmenu(flaga, flagb);
+                maininput = int.Parse(Console.ReadLine());
+                switch (maininput)
+                {
+                    case 1:
+
+                        break;
+
+
+
+                    default:
+                        break;
+                }
+            }
+            catch(Exception)
+            {
+                mainclass ex = new mainclass();
+                ex.invalidinput();
+                goto main;
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 2-D arrays/2-D arrays/mainclass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_D_arrays
+{
+    class mainclass
+    {
+
+        public void mainmenu(int flag1, int flagb)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("===================================MAIN MENU===================================");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nPress 1 to Create a Matrix");
+            if (flag1 == 1 || flagb == 1)
+            {
+                Console.WriteLine("\nPress 2 to Insert element in Matrix");
+                Console.WriteLine("\nPress 3 to Replace element in Matrix");
+                Console.WriteLine("\nPress 5 to Search element in Matrix");                
+                Console.WriteLine("\nPress 7 to Display the Matrix");
+                Console.WriteLine("\nPress 8 to Switiching  Matrix");
+            }
+          
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("select a no. to perform the following the funtion: ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+        } //1st display method
+
+        public void invalidinput()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input!!!!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("press any key to go back......");
+            Console.ReadKey();
+            Console.Clear();
+        } //exception method
+    
+    
+    }
+}
